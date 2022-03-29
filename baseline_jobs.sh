095c156de09aac2e7f5c82de44a9d7254bd64e0a
Arda Arslan <aarslan@ethz.ch>
2022-03-29 12:13:04 +0200

    Ready to submit

--- baseline_jobs.sh
+++ baseline_jobs.sh
@@ -1,7 +1,7 @@
 cd src/baselines
 
 # Baseline CNN for MIT-BIH dataset
-bsub -n 2 -W 4:00 -R "rusage[mem=4096, ngpus_excl_p=1]" python baseline_mitbih.py
+python baseline_mitbih.py
 
 # Baseline CNN for PTBDB dataset
-bsub -n 2 -W 4:00 -R "rusage[mem=4096, ngpus_excl_p=1]" python baseline_ptbdb.py
+python baseline_ptbdb.py
--- ensemble_jobs.sh
+++ ensemble_jobs.sh
@@ -1,14 +1,14 @@
 cd src
 
 # Ensemble: Majority Voting for MIT-BIH dataset
-bsub -n 2 -W 4:00 -R "rusage[mem=4096, ngpus_excl_p=1]" python ensemble.py --dataset_name mitbih --model_name majority_ensemble
+python ensemble.py --dataset_name mitbih --model_name majority_ensemble
 
 # Ensemble: Logistic Regression for MIT-BIH dataset
-bsub -n 2 -W 4:00 -R "rusage[mem=4096, ngpus_excl_p=1]" python ensemble.py --dataset_name mitbih --model_name log_reg_ensemble
+python ensemble.py --dataset_name mitbih --model_name log_reg_ensemble
 
 # Ensemble: Majority Voting for PTBDB dataset
-bsub -n 2 -W 4:00 -R "rusage[mem=4096, ngpus_excl_p=1]" python ensemble.py --dataset_name ptbdb --model_name majority_ensemble
+python ensemble.py --dataset_name ptbdb --model_name majority_ensemble
 
 # Ensemble: Logistic Regression for PTBDB dataset
-bsub -n 2 -W 4:00 -R "rusage[mem=4096, ngpus_excl_p=1]" python ensemble.py --dataset_name ptbdb --model_name log_reg_ensemble
+python ensemble.py --dataset_name ptbdb --model_name log_reg_ensemble
 
